58968870d126117217d1d3eb619191e894bbee53
Harsh Mahyavanshi <harshmahyavanshi940@gmail.com>
2020-02-14 14:43:38 +0530

    first commit

---
+++ app/src/main/java/com/example/ecommerce/Prevalent/Prevalent.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.Prevalent;
+
+import com.example.ecommerce.Model.Users;
+
+public class Prevalent
+{
+    private static Users currentOnlineUsers;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+
+}
